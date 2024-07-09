7b761dc14c6deb47c373199cc6986234325f6ea5
pp.navillera <pp.navillera@gmail.com>
2024-07-09 19:12:00 +0900

    Fix: Notion API Pagination

--- supabase/functions/esmp/index.ts
+++ supabase/functions/esmp/index.ts
@@ -7,43 +7,36 @@ serve(async (req: Request) => {
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
   }
+
   try {
     const NOTION_TOKEN = Deno.env.get("NOTION_TOKEN");
     const NOTION_DATABASE_ID = Deno.env.get("NOTION_DATABASE_ID");
     const { query } = await req.json();
 
-    const notionResponse = await axios.post(
-      `https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`,
-      query,
-      {
-        headers: {
-          Authorization: `Bearer ${NOTION_TOKEN}`,
-          "Content-Type": "application/json",
-          "Notion-Version": "2022-06-28",
+    let hasMore = true;
+    let startCursor = undefined;
+    let allPages: any[] = [];
+
+    while (hasMore) {
+      const notionResponse = await axios.post(
+        `https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`,
+        { ...query, start_cursor: startCursor },
+        {
+          headers: {
+            Authorization: `Bearer ${NOTION_TOKEN}`,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28",
+          },
         },
-      },
-    );
+      );
 
-    const data = notionResponse.data;
+      const data: any = notionResponse.data;
+      allPages = allPages.concat(data.results);
+      hasMore = data.has_more;
+      startCursor = data.next_cursor;
+    }
 
-    // const songs = data.results.map(
-    //   (
-    //     page: {
-    //       properties: {
-    //         Link: { url: any };
-    //         Song: { title: { text: { content: any } }[] };
-    //       };
-    //     },
-    //   ) => {
-    //     console.log(page.properties.Link.url);
-    //     return {
-    //       name: page.properties.Song.title[0].text.content,
-    //       url: page.properties.Link.url,
-    //     };
-    //   },
-    // );
-    // console.log(data);
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify({ results: allPages }), {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
     });
